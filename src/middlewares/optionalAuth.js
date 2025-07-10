54aecafe96669135fcd6602a0f02590980dc9922
lucas leiro <lucasleiroa@gmail.com>
2025-07-10 14:48:32 -0300

    saving

--- src/middlewares/optionalAuth.js
+++ src/middlewares/optionalAuth.js
@@ -1,12 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-/**
- * Middleware que decodifica el JWT si está presente,
- * pero no bloquea el acceso si no hay token válido.
- *
- * Si el token es válido, agrega `req.user = { id, email, role, dealerId }`
- * Si no hay token o es inválido, continúa sin errores.
- */
+
 
 
 const optionalAuth = (req, res, next) => {
